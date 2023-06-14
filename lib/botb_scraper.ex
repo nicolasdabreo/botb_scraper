c34b87d6ea681df4859454cbef6ca9c3424449ca
Nicolas Dabreo <nicolas@megalixir.co.uk>
2023-06-14 19:49:10 +0200

    :tada: Make it so

---
+++ lib/botb_scraper.ex
@@ -0,0 +1,18 @@
+defmodule BotbScraper do
+  @moduledoc """
+  Documentation for `BotbScraper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BotbScraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/botb_scraper_test.exs
@@ -0,0 +1,8 @@
+defmodule BotbScraperTest do
+  use ExUnit.Case
+  doctest BotbScraper
+
+  test "greets the world" do
+    assert BotbScraper.hello() == :world
+  end
+end
