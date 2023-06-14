fb9ce70154b5bf18ef300841fb5b739f3ae41dbf
Nicolas Dabreo <nicolas@megalixir.co.uk>
2023-06-14 19:51:06 +0200

    :heavy_plus_sign: Add crawly and floki

--- mix.exs
+++ mix.exs
@@ -11,18 +11,16 @@ defmodule BotbScraper.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:crawly, "~> 0.15"},
+      {:floki, "~> 0.34"}
     ]
   end
 end
